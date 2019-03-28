4a080af99ffda71b4c819cc8f4709e3eea2cd9e9
Yuu Fuerugen <furugen098@gmail.com>
2019-03-28 13:56:52 +0900

    サンプルコミット

---
+++ wpf-samples-busyindicator/wpf-samples-busyindicator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_samples_busyindicator
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            xamBusyIndicator.IsBusy = true;
+            this.Dispatcher.BeginInvoke((Action)(async () =>
+            {
+                this.textJson.Text = await GetRequest(this.jsonUrl.Text);
+                xamBusyIndicator.IsBusy = false;
+            }));
+        }
+
+        private async Task<string> GetRequest(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+
+            return null;
+        }
+    }
+}
